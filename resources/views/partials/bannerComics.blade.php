bcd039bda34011636c1b8dbacbf3f5a0523fd01e
Anna93803 <anna.tassone93@gmail.com>
2022-07-26 17:50:44 +0200

    add banner

--- resources/views/partials/bannerComics.blade.php
+++ resources/views/partials/bannerComics.blade.php
@@ -1,3 +1,34 @@
+
+<?php
+$linkBanner = [
+    [
+        'name' => 'Digital Comics',
+        'src' => 'buy-comics-digital-comics.png',
+        'href' =>'#'
+    ],
+    [
+        'name' => 'DC Merchandise',
+        'src' => 'buy-comics-merchandise.png',
+        'href' => '#'
+    ],
+    [
+        'name' => 'Subscription',
+        'src' => 'buy-comics-subscriptions.png',
+        'href' => '#'
+    ],
+    [
+        'name' => 'Comics Shop locator',
+        'src' => 'buy-comics-shop-locator.png',
+        'href' => '#'
+    ],
+    [
+        'name' => 'Dc Power Visa',
+        'src' => 'buy-dc-power-visa.svg',
+        'href' => '#'
+    ]
+];
+?>
+
 <div class="bg-container-cards">
     <div class="container">
         <div class="row">
@@ -15,6 +46,22 @@
             <div class="col">
                 <button class="btn">LOAD MORE</button>
             </div>
+        </div>
+    </div>
+</div>
+<div class="bg-banner">
+    <div class="container">
+        <div class="banner-cards">
+            <ul>
+                @foreach ($linkBanner as $link)
+                <li>
+                    <a href="{{ $link['href']}}">
+                        <img src="{{ asset( 'images/' . $link['src'])}}" alt="{{$link['name']}}">
+                        <h4>{{ $link['name']}}</h4>
+                    </a>
+                </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 </div>
